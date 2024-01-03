88eac336160f0a95af51400dd5b13a2b595dfc22
Patrick Boos <patrickboos85@gmail.com>
2024-01-03 21:43:43 +0100

    Add tailwind & daisyui

--- error.vue
+++ error.vue
@@ -6,7 +6,7 @@ const handleError = () => clearError({ redirect: '/' })
 
 <template>
   <div>
-    <h2>{{ error.statusCode }}</h2>
+    <h2 class="text-3xl font-bold underline">{{ error.statusCode }}</h2>
     <button @click="handleError">Clear errors</button>
   </div>
 </template>
