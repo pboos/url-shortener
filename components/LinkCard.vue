2eff9135535eac74fb3ef7e6faaa022d0a5346f2
Patrick Boos <patrickboos85@gmail.com>
2024-01-04 14:32:46 +0100

    card hover & click

--- components/LinkCard.vue
+++ components/LinkCard.vue
@@ -1,23 +1,21 @@
 <script lang="ts" setup>
-import type { Link } from "~/model/api/link";
+import type {Link} from "~/model/api/link";
 
-defineProps<{
+const props = defineProps<{
   link: Link;
 }>();
+
+const navigateToLink = () => {
+  navigateTo({ name: 'admin-key', params: { key: props.link.key } })
+};
 </script>
 
 <template>
-  <div class="card w-96 bg-base-100 shadow-xl">
+  <div class="card hover:bg-gray-100 w-96 bg-base-100 shadow-xl cursor-pointer" @click="navigateToLink">
     <div class="card-body">
+      <h2 class="card-title">/{{ link.key }}</h2>
       <div class="flex justify-between gap-5 transition-all duration-200">
         <div class="w-1/2">
-          <div class="text-xl">
-            <NuxtLink
-              :to="{ name: 'admin-key', params: { key: link.key } }"
-              class="text-amber-500 font-bold"
-              >/{{ link.key }}
-            </NuxtLink>
-          </div>
           <div class="text-xs sm:text-base">
             {{ link.url.slice(0, 20) + "..." }}
           </div>
@@ -29,24 +27,24 @@ defineProps<{
                 {{ link.totalVisits }}
               </span>
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-4 h-4 sm:w-6 sm:h-6 ml-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-4 h-4 sm:w-6 sm:h-6 ml-2"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
                 />
               </svg>
             </div>
             <div class="">
               <span class="text-xs sm:text-sm">{{
-                new Date(link.createdAt).toDateString()
-              }}</span>
+                  new Date(link.createdAt).toDateString()
+                }}</span>
             </div>
           </div>
           <!--      <CopyBtn :data="config.public.appUrl + '/' + link.key" />-->
